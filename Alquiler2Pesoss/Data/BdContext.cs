859bce8777dc1ecd5849239ca063f6ef45b2b702
tinirusta10 <martinirusta10@hotmail.com>
2022-06-06 23:45:47 -0300

    Cree entidades

---
+++ Alquiler2Pesoss/Data/BdContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Alquiler2Pesoss.Data
+{
+    public class BdContext :DbContext
+    {
+        public BdContext(DbContextOptions<BdContext> options) : base(options) { }
+
+        
+
+    }
+}
---
+++ Alquiler2Pesoss/Entidades/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Alquiler2Pesoss.Entidades
+{
+    public class Producto
+    {
+        [Required]
+        public int id { get; set; }
+
+        public string Nombre { get; set; }
+
+
+
+
+    }
+
+
+}
