b5be8508ebbc1520aa8eaf2caae681e68fb6d24d
tinirusta10 <martinirusta10@hotmail.com>
2022-06-08 10:43:52 -0300

    agregue contenido a base

---
+++ Alquiler2Pesoss/ScriptBD/dbo.TablaLocalidad.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[TablaLocalidad] (
+    [Id]              INT            IDENTITY (1, 1) NOT NULL,
+    [NombreLocalidad] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_TablaLocalidad] PRIMARY KEY CLUSTERED ([Id] ASC), 
+   
+);
+
