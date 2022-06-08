ccbacdb771f50f300a357accde5a4c3a25555d69
santiago moyano ministro <san_moyano@hotmail.com>
2022-06-08 18:11:24 -0300

    Agregado libreria de clases llamado Blazor.Crud.Model Agregado Componente de Razon con nombre FilmDetails Agregado Libreria de clases llamado BlazorCRUD.Data.Dapper Nueva Carpeta "interfaces" Nueva Carpeta "servicios"

---
+++ BlazorCRUD.Model/Film.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.Model
+{
+    public class Film
+    {
+        public int Id { get; set; }
+        public string nombre_producto { get; set; }
+        public string precio_producto { get; set; }
+        public string descripcion_producto { get; set; }
+    }
+}
