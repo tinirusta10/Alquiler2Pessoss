61de89903eec2dde72f2fa2527501fccbb03c32e
tinirusta10 <martinirusta10@hotmail.com>
2022-06-11 00:05:25 -0300

    guardo repo

--- Alquiler2Pesoss/ScriptBD/dbo.TablaLocalidad.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[TablaLocalidad] (
-    [Id]              INT            IDENTITY (1, 1) NOT NULL,
-    [NombreLocalidad] NVARCHAR (MAX) NULL,
-    CONSTRAINT [PK_TablaLocalidad] PRIMARY KEY CLUSTERED ([Id] ASC), 
-   
-);
-
