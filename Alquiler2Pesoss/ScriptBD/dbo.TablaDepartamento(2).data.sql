03d132c8558078fd26657d2b323428695ec01f35
tinirusta10 <martinirusta10@hotmail.com>
2022-06-10 22:56:55 -0300

    agregue scrip

--- Alquiler2Pesoss/ScriptBD/dbo.TablaDepartamento(2).data.sql
+++
@@ -1,4 +0,0 @@
-﻿SET IDENTITY_INSERT [dbo].[TablaDepartamento] ON
-INSERT INTO [dbo].[TablaDepartamento] ([Id], [NombreDepartamento]) VALUES (1, N'Cordoba')
-INSERT INTO [dbo].[TablaDepartamento] ([Id], [NombreDepartamento]) VALUES (2, N'')
-SET IDENTITY_INSERT [dbo].[TablaDepartamento] OFF
---
+++ Alquiler2Pesoss/ScriptBD/dbo.TablaTipoDocumento.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[TablaTipoDocumento] (
+    [Id]                  INT            IDENTITY (1, 1) NOT NULL,
+    [NombreTipoDocumento] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_TablaTipoDocumento] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
