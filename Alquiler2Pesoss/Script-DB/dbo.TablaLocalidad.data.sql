38c6412c9e069d07d04970aaac38efae6e9675bf
tinirusta10 <martinirusta10@hotmail.com>
2022-06-11 11:42:19 -0300

    Agregue registro

---
+++ Alquiler2Pesoss/Script-DB/dbo.TablaLocalidad.data.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[TablaLocalidad] ON
+INSERT INTO [dbo].[TablaLocalidad] ([Id], [NombreLocalidad]) VALUES (1, N'villa allende')
+SET IDENTITY_INSERT [dbo].[TablaLocalidad] OFF
