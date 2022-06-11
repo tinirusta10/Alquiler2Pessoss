892652e6b7c7da6869fde72ac32c43d1ee344706
IsaacVGutierrez <90696341+IsaacVGutierrez@users.noreply.github.com>
2022-06-11 00:55:42 -0300

    kkk

--- dbo.TablaProvincia.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[TablaProvincia] (
-    [Id]              INT            IDENTITY (1, 1) NOT NULL,
-    [NombreProvincia] NVARCHAR (MAX) NULL,
-    [cordoba] NCHAR(10) NULL, 
-    CONSTRAINT [PK_TablaProvincia] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
